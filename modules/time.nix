00629e9d9df38377427ec9ea5f97884940389efd
Evan Assmus <evanwassmus@gmail.com>
2024-12-26 00:52:41 -0700

    fixed auto timezones

--- modules/time.nix
+++ modules/time.nix
@@ -2,5 +2,13 @@
 
 {
   services.automatic-timezoned.enable = true;
+  services.geoclue2 = {
+    enable = true;
+    geoProviderUrl = "https://api.positon.xyz/v1/geolocate?key=test";
+    submissionUrl = "https://api.positon.xyz/v2/geosubmit?key=test";
+    submitData = false;
+  };
+  services.localtimed.enable = true;
+
   time.hardwareClockInLocalTime = true;
 }
