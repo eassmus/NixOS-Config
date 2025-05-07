211247275116743116d6fe4ce7302131ba8c2f07
Evan Assmus <evanwassmus@gmail.com>
2025-05-06 23:12:44 -0500

    flakes

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "My NixOS Config!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos-personal = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
