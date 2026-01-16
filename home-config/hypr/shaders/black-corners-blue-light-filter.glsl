00bbb7ec907f4f0e3361d36897d8c857c83d2865
Evan Assmus <evanwassmus@gmail.com>
2026-01-15 22:47:36 -0500

    fix shader corenrs for any size monitor

--- home-config/hypr/shaders/black-corners-blue-light-filter.glsl
+++ home-config/hypr/shaders/black-corners-blue-light-filter.glsl
@@ -1,13 +1,14 @@
+#version 320 es
+// GLSL Fragment Shader
 // GLSL Fragment Shader
 
 #ifdef GL_ES
 precision highp float;
 #endif
 
-varying vec2 v_texcoord;
+in vec2 v_texcoord;
 uniform sampler2D tex;
-
-uniform vec2 resolution;
+out vec4 fragColor;
 
 const float temperature = 2600.0;
 const float temperatureStrength = 0.6;
@@ -25,24 +26,29 @@ vec3 colorTemperatureToRGB(const in float temperature) {
 }
 
 void main() {
-  vec4 pixColor = texture2D(tex, v_texcoord);
+  vec4 pixColor = texture(tex, v_texcoord);
   vec3 color = vec3(pixColor[0], pixColor[1], pixColor[2]);
 
-  vec2 scaled_loc = v_texcoord * vec2(2560.0, 1440.0);
+  ivec2 size = textureSize(tex, 0);
+
+  float screenWidth = float(size.x);
+  float screenHeight = float(size.y);
+
+  vec2 scaled_loc = v_texcoord * vec2(screenWidth, screenHeight);
 
   vec2 top_left = vec2(0.0, 0.0);
-  vec2 top_right = vec2(2560.0, 0.0);
-  vec2 bottom_left = vec2(0.0, 1440.0);
-  vec2 bottom_right = vec2(2560.0, 1440.0);
+  vec2 top_right = vec2(screenWidth, 0.0);
+  vec2 bottom_left = vec2(0.0, screenHeight);
+  vec2 bottom_right = vec2(screenWidth, screenHeight);
 
   float min_distance = min(min(distance(scaled_loc, top_left), distance(scaled_loc, top_right)), min(distance(scaled_loc, bottom_left), distance(scaled_loc, bottom_right)));
 
   float radius = 20.0;
 
   vec2 top_left_center2 = vec2(radius, radius);
-  vec2 top_right_center2 = vec2(2560.0 - radius, radius);
-  vec2 bottom_left_center2 = vec2(radius, 1440.0 - radius);
-  vec2 bottom_right_center2 = vec2(2560.0 - radius, 1440.0 - radius);
+  vec2 top_right_center2 = vec2(screenWidth - radius, radius);
+  vec2 bottom_left_center2 = vec2(radius, screenHeight - radius);
+  vec2 bottom_right_center2 = vec2(screenWidth - radius, screenHeight - radius);
 
   float min_distance3 = min(min(distance(scaled_loc, top_left_center2), distance(scaled_loc, top_right_center2)), min(distance(scaled_loc, bottom_left_center2), distance(scaled_loc, bottom_right_center2)));
 
@@ -54,5 +60,5 @@ void main() {
   color = vec3(color[0], color[1], color[2] * 0.7);
   vec4 outCol = vec4(0.75 * color, pixColor[3]);
 
-  gl_FragColor = outCol;
+  fragColor = outCol;
 }
--- home-config/hypr/shaders/black-corners.glsl
+++ home-config/hypr/shaders/black-corners.glsl
@@ -1,33 +1,38 @@
+#version 320 es
 // GLSL Fragment Shader
 
 #ifdef GL_ES
 precision highp float;
 #endif
 
-varying vec2 v_texcoord;
+in vec2 v_texcoord;
 uniform sampler2D tex;
-
-uniform vec2 resolution;
+out vec4 fragColor;
 
 void main() {
-  vec4 pixColor = texture2D(tex, v_texcoord);
+  vec4 pixColor = texture(tex, v_texcoord);
   vec3 color = vec3(pixColor[0], pixColor[1], pixColor[2]);
 
-  vec2 scaled_loc = v_texcoord * vec2(2560.0, 1440.0);
+  ivec2 size = textureSize(tex, 0);
+
+  float screenWidth = float(size.x);
+  float screenHeight = float(size.y);
+
+  vec2 scaled_loc = v_texcoord * vec2(screenWidth, screenHeight);
 
   vec2 top_left = vec2(0.0, 0.0);
-  vec2 top_right = vec2(2560.0, 0.0);
-  vec2 bottom_left = vec2(0.0, 1440.0);
-  vec2 bottom_right = vec2(2560.0, 1440.0);
+  vec2 top_right = vec2(screenWidth, 0.0);
+  vec2 bottom_left = vec2(0.0, screenHeight);
+  vec2 bottom_right = vec2(screenWidth, screenHeight);
 
   float min_distance = min(min(distance(scaled_loc, top_left), distance(scaled_loc, top_right)), min(distance(scaled_loc, bottom_left), distance(scaled_loc, bottom_right)));
 
   float radius = 20.0;
 
   vec2 top_left_center2 = vec2(radius, radius);
-  vec2 top_right_center2 = vec2(2560.0 - radius, radius);
-  vec2 bottom_left_center2 = vec2(radius, 1440.0 - radius);
-  vec2 bottom_right_center2 = vec2(2560.0 - radius, 1440.0 - radius);
+  vec2 top_right_center2 = vec2(screenWidth - radius, radius);
+  vec2 bottom_left_center2 = vec2(radius, screenHeight - radius);
+  vec2 bottom_right_center2 = vec2(screenWidth - radius, screenHeight - radius);
 
   float min_distance3 = min(min(distance(scaled_loc, top_left_center2), distance(scaled_loc, top_right_center2)), min(distance(scaled_loc, bottom_left_center2), distance(scaled_loc, bottom_right_center2)));
 
@@ -39,5 +44,5 @@ void main() {
   pixColor[1] = color[1];
   pixColor[2] = color[2];
 
-  gl_FragColor = pixColor;
+  fragColor = pixColor;
 }
